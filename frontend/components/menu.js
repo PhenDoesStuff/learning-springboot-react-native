9e8a5e3e5e806353a9cf4495aae3abc12d8cb295
Stephen Montague <Stephen.D.Montague@gmail.com>
2022-04-08 07:06:41 -0400

    Formatting

--- frontend/components/menu.js
+++ frontend/components/menu.js
@@ -1,46 +1,46 @@
-import react from "react";
-import { createStackNavigator } from "@react-navigation/stack";
-import { createDrawerNavigator } from "@react-navigation/drawer";
-import { NavigationContainer } from "@react-navigation/native";
-import { Icon } from "@rneui/base";
+import react from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
+import {NavigationContainer} from '@react-navigation/native';
+import {Icon} from '@rneui/base';
 
 import HomePage from '../pages/HomePage';
 import ProductPage from '../pages/ProductPage';
-import DetailPage from '../pages/DetailPage'
+import DetailPage from '../pages/DetailPage';
 
-const ListStack = createStackNavigator({
-  Product: {screen: ProductPage},
-  Detail: {screen: DetailPage},
-},
-{
-  initialRouteName: 'Product'
-});
+const ListStack = createStackNavigator(
+  {
+    Product: {screen: ProductPage},
+    Detail: {screen: DetailPage},
+  },
+  {
+    initialRouteName: 'Product',
+  },
+);
 
 const Menu = createDrawerNavigator(
   {
-    "Home": {
+    Home: {
       screen: HomePage,
       navigationOptions: ({navigation}) => ({
-        drawerIcon: ({tintColor}) => (
-          <Icon name="home" color={tintColor} />
-        )
+        drawerIcon: ({tintColor}) => <Icon name="home" color={tintColor} />,
       }),
     },
-    "Product": {
+    Product: {
       screen: ListStack,
       navigationOptions: ({navigation}) => ({
         drawerIcon: ({tintColor}) => (
           <Icon name="local-grocery-store" color={tintColor} />
         ),
       }),
-    }
+    },
   },
   {
     drawerWidth: 300,
     drawerPosition: 'left',
-    initialRouteName: 'Home'
-  }
+    initialRouteName: 'Home',
+  },
 );
 
-const MenuStack = NavigationContainer(Menu)
+const MenuStack = NavigationContainer(Menu);
 export default MenuStack;
